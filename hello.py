bc004cb5855409b37a9c9e779384e3578629b917
root <root@wapp-application.northamerica-northeast2-a.c.weatherappv2-417003.internal>
2024-03-13 07:03:19 +0000

    First Commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+print ("Content-Type: text/html")
+print()
+print ("<html><head>")
+print ("</head><body>")
+print ("WeatherApp v2 In Progress ...")
+print ("</body></html>")
---
+++ index.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+html_content = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Weather in Tokyo</title>
+    <style>
+        body {
+            background-color: #f0f0f0;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin-top: 100px;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            color: #555;
+        }
+    </style>
+</head>
+<body>
+    <h1>Weather in Tokyo</h1>
+    <p>Description: Sunny</p>
+    <p>Temperature: 25°C</p>
+    <p>Humidity: 60%</p>
+</body>
+</html>
+"""
+
+# Printing the HTML content
+print(html_content)
